91ac839cb05c83e54363833ed10d1ea5ce94b408
Mohamed Leila <mohamed.laila@mail.mcgill.ca>
2021-01-21 09:39:06 -0500

    [NEW] add DecoderRNN

--- attention/models.py
+++ attention/models.py
@@ -19,3 +19,70 @@ class Encoder(nn.Module):
 
     def init_hidden(self, batch_size, device='cpu'):
         return torch.zeros(1, batch_size, self.hidden_size, device=device)
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, embedding_matrix, hidden_size, output_size):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding.from_pretrained(embedding_matrix)
+        vocab_size, embedding_size = embedding_matrix.shape
+        self.gru = nn.GRU(embedding_size, hidden_size, batch_first=True)
+
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input)
+        output = torch.relu(output)
+        output = output.unsqueeze(1)
+
+        # input (batch, seq_len, input_size)
+        output, hidden = self.gru(output, hidden)
+        # output (batch, 1, hidden)
+        output = output.squeeze(1) # output (batch, hidden)
+        output = self.softmax(self.out(output))
+        return output, hidden
+
+    def init_hidden(self, batch_size, device):
+        return torch.zeros(1, batch_size, self.hidden_size, device=device)
+
+
+class TranslationModel(nn.Module):
+
+    def __init__(self, encoder, decoder, output_vocab_size):
+        super(TranslationModel, self).__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.output_vocab_size = output_vocab_size
+
+    def forward(self, encoder_input, decoder_input, device='cpu'):
+        """
+        Model forward
+
+        encoder_input: (batch_size, seq_len)
+        decoder_input: (batch_size, seq_len)
+        """
+        # encoder setup
+        batch_size, seq_len = encoder_input.shape
+        encoder_hidden = self.encoder.init_hidden(batch_size)
+
+        # encoder forward pass
+        encoder_output, encoder_hidden = self.encoder(encoder_input, encoder_hidden)
+        encoder_output = encoder_output.to(device)
+        encoder_hidden = encoder_hidden.to(device)
+
+        # decoder setup
+        decoder_hidden = encoder_hidden
+        decoder_outputs = torch.zeros(seq_len, batch_size, self.output_vocab_size)
+
+        # decoder forward pass
+        for index in range(seq_len-1):
+            # select input tokens (batch_size, 1)
+            current_tokens = decoder_input[:, index]
+            decoder_output, decoder_hidden = self.decoder(current_tokens, decoder_hidden)
+            decoder_outputs[index] = decoder_output
+
+        return decoder_outputs.view(batch_size, seq_len, self.output_vocab_size)
