82c15470e00dd0788b7c51b2f466d172eb764281
Mohamed Leila <mohamed.laila@mail.mcgill.ca>
2021-01-25 18:39:38 -0500

    [FIX] fix logic in encoder and decoder

--- attention/models.py
+++ attention/models.py
@@ -2,52 +2,59 @@ import torch
 from torch import nn
 
 
-class Encoder(nn.Module):
-    def __init__(self, embedding_matrix, hidden_size):
-        super(Encoder, self).__init__()
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, embedding_size=None, embedding_matrix=None):
+        super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
 
-        self.embedding = nn.Embedding.from_pretrained(embedding_matrix)
+        # embedding layer
+        if embedding_matrix is None:
+            self.embedding = nn.Embedding(input_size, embedding_size)
+        else:
+            embedding_size = embedding_matrix.shape[-1]
+            self.embedding = nn.Embedding.from_pretrained(embedding_matrix)
 
-        _, embedding_size = embedding_matrix.shape
-        self.gru = nn.GRU(embedding_size, hidden_size, batch_first=True)
+        # rnn layer
+        self.gru = nn.GRU(embedding_size, hidden_size)
 
     def forward(self, input, hidden):
+
         embedded = self.embedding(input)
-        output = embedded
+        output = embedded.unsqueeze(0)
+        # GRU input (seq_len, batch, input_size)
+        # GRU hidden (num_layers * num_directions, batch, hidden_size)
         output, hidden = self.gru(output, hidden)
         return output, hidden
 
-    def init_hidden(self, batch_size, device='cpu'):
+    def initHidden(self, batch_size, device):
+        # (num_layers * num_directions, batch_size, hidden_size)
         return torch.zeros(1, batch_size, self.hidden_size, device=device)
 
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embedding_matrix, hidden_size, output_size):
+    def __init__(self, hidden_size, output_size, embedding_size=None, embedding_matrix=None):
         super(DecoderRNN, self).__init__()
         self.hidden_size = hidden_size
+        self.output_size = output_size
 
-        self.embedding = nn.Embedding.from_pretrained(embedding_matrix)
-        vocab_size, embedding_size = embedding_matrix.shape
-        self.gru = nn.GRU(embedding_size, hidden_size, batch_first=True)
+        # embedding layer
+        if embedding_matrix is None:
+            self.embedding = nn.Embedding(output_size, embedding_size)
+        else:
+            embedding_size = embedding_matrix.shape[-1]
+            self.embedding = nn.Embedding.from_pretrained(embedding_matrix)
 
+        # rnn layer
+        self.gru = nn.GRU(embedding_size, hidden_size)
         self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.Softmax(dim=1)
+        self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input, hidden):
         output = self.embedding(input)
         output = torch.relu(output)
-        output = output.unsqueeze(1)
-
-        # input (batch, seq_len, input_size)
         output, hidden = self.gru(output, hidden)
-        # output (batch, 1, hidden)
-        output = output.squeeze(1) # output (batch, hidden)
-        output = self.softmax(self.out(output))
+        output = self.softmax(self.out(output[0]))
         return output, hidden
-
-    def init_hidden(self, batch_size, device):
-        return torch.zeros(1, batch_size, self.hidden_size, device=device)
 
 
 class TranslationModel(nn.Module):
