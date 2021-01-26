bbb949323bceecd75410eb7f7d91e1d775b01700
Mohamed Leila <mohamed.laila@mail.mcgill.ca>
2021-01-25 20:28:07 -0500

    [NEW] implement new translation function

--- attention/utils.py
+++ attention/utils.py
@@ -29,12 +29,40 @@ def tokenize_english(sentence):
 def tokenize_french(sentence):
     return sentence.split(" ")
 
-def translate(sentence, model, vectorizer):
+def translate_simple_rnn(sentence, encoder, decoder, vectorizer, device):
+    batch_size = 1
+    english_vector = vectorizer.vectorize_sentence(sentence, language=ENGLISH).to(device)
     french_vocab = vectorizer.french_vocab
-    encoder_input = vectorizer.vectorize_sentence(sentence, language=ENGLISH).unsqueeze(0)
-    model.eval()
-    sos_token = french_vocab.lookup_token(french_vocab.sos)
-    prediction = model(encoder_input, sos_token=sos_token).squeeze(0)
-    word_indices = torch.argmax(prediction, dim=1)
-    words = [vectorizer.french_vocab.lookup_index(idx) for idx in word_indices]
+    seq_size = len(english_vector)
+
+    sos_token = vectorizer.french_vocab.sos
+    eos_token = vectorizer.french_vocab.eos
+
+    encoder_hidden = encoder.initHidden(batch_size, device)
+    for idx in range(seq_size):
+        token = english_vector[idx].unsqueeze(0)
+        _, encoder_hidden = encoder(token, encoder_hidden)
+
+    decoder_outputs = torch.zeros(seq_size-1, decoder.output_size)
+
+    hidden = encoder_hidden
+    sos_token_index = int(french_vocab.lookup_token(sos_token))
+    eos_token_index = int(french_vocab.lookup_token(eos_token))
+    output = torch.tensor([sos_token_index], dtype=torch.int64).view(1, 1)
+
+    indices = []
+    for idx in range(seq_size-1):
+        token = english_vector[idx].view(batch_size, 1)
+        token_word = french_vocab.lookup_index(token[0].item())
+        if token_word == eos_token:
+            break
+
+        output, hidden =  decoder(output, hidden)
+        output = torch.argmax(output, dim=-1).view(1, 1)
+        if output.squeeze(0).item() == eos_token_index:
+            break
+        indices.append(output.squeeze(0).item())
+        decoder_outputs[idx] = output.squeeze(0)
+
+    words = [french_vocab.lookup_index(idx) for idx in indices]
     return ' '.join(words)
